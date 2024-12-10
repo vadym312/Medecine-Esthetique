4f5d470def3e999beac48eebab07332df6a6fa67
vadym312 <vadym.maksymuik@outlook.com>
2024-12-10 17:46:20 +0200

    updated skinbooster page

--- app/injections/soins/skinbooster/page.tsx
+++ app/injections/soins/skinbooster/page.tsx
@@ -4,9 +4,6 @@ import React from 'react';
 import { TreatmentHero } from '@/src/components/shared/treatments/Hero';
 import { TreatmentTypes } from '@/src/components/shared/treatments/Types';
 import { TreatmentProcedure } from '@/src/components/shared/treatments/Procedure';
-import { TreatmentVideo } from '@/src/components/shared/treatments/Video';
-import { TreatmentResults } from '@/src/components/shared/treatments/Results';
-import { TreatmentTestimonials } from '@/src/components/shared/treatments/Testimonials';
 import { TreatmentFAQ } from '@/src/components/shared/treatments/FAQ';
 import { TreatmentCTA } from '@/src/components/shared/treatments/CTA';
 import { RelatedProcedures } from '@/src/components/shared/treatments/RelatedProcedures';
@@ -25,9 +22,6 @@ const SkinboosterPage: React.FC = () => {
 
       <TreatmentTypes {...skinboosterData.types} />
       <TreatmentProcedure {...skinboosterData.procedure} />
-      <TreatmentVideo {...skinboosterData.video} />
-      <TreatmentResults {...skinboosterData.results} />
-      <TreatmentTestimonials {...skinboosterData.testimonials} />
       <TreatmentFAQ {...skinboosterData.faq} />
       <TreatmentCTA {...skinboosterData.cta} />
       <RelatedProcedures {...skinboosterData.relatedProcedures} />
